fa12a04353a4887a7a9dae913414d26474903a05
Seongho Bae <me@seonghobae.me>
2026-09-07 17:08:44 +0900

    repair: withdraw unmeasured llcont optimization

--- R/llcont.R
+++ R/llcont.R
@@ -53,16 +53,12 @@ llcont.glm <- function(x, ...){
              if(is.matrix(y)) {
                ## Bolt: replaced apply(..., 1, sum) with optimized rowSums() for performance
                n <- rowSums(y)
-               y_tmp <- y[, 1] / n
-               y_tmp[n == 0] <- 0
-               y <- y_tmp
+               y <- ifelse(n == 0, 0, y[, 1]/n)
              } else {
                n <- rep.int(1, length(y))
              }
              m <- if (any(n > 1)) n else wt
-             wt_tmp <- wt / m
-             wt_tmp[m <= 0] <- 0
-             wt <- wt_tmp
+             wt <- ifelse(m > 0, (wt/m), 0)
              dbinom(round(m * y), round(m), mpreds, log = TRUE) * wt
            },
            quasibinomial = {
